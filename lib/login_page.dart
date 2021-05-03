78e346ed6c92649786feac6d785b614626e27acc
Cesar Alves de Azevedo <cesarazevedocmd@gmail.com>
2021-05-03 17:05:52 -0300

    Creating TextFormField

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,11 +7,26 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Cars"), centerTitle: true),
-      body: _body(),
+      body: Padding(padding: EdgeInsets.all(16), child: _body()),
     );
   }
 
   _body() {
-    return Container();
+    return ListView(
+      children: [
+        Text("Login"),
+        TextFormField(),
+        SizedBox(height: 10),
+        Text("Password"),
+        TextFormField(obscureText: true),
+        SizedBox(height: 10),
+        ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              "LOGIN",
+              style: TextStyle(fontSize: 18),
+            ))
+      ],
+    );
   }
 }
