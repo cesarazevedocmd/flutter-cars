84d0a25588e52d3f7af54f46796f23fdb6896557
Cesar Alves de Azevedo <cesarazevedocmd@gmail.com>
2021-05-04 15:31:33 -0300

    Creating Try Catch

--- lib/pages/login_api.dart
+++ lib/pages/login_api.dart
@@ -6,21 +6,26 @@ import 'package:http/http.dart' as http;
 
 class LoginApi {
   static Future<ApiResponse<User>> login(String login, String password) async {
-    var url = "https://carros-springboot.herokuapp.com/api/v2/login";
+    try{
+      var url = "https://carros-springboot.herokuapp.com/api/v2/login";
 
-    Map params = {"username": login, "password": password};
+      Map params = {"username": login, "password": password};
 
-    Map<String, String> headers = {"Content-Type": "application/json"};
+      Map<String, String> headers = {"Content-Type": "application/json"};
 
-    var result = await http.post(Uri.parse(url), headers: headers, body: json.encode(params));
+      var result = await http.post(Uri.parse(url), headers: headers, body: json.encode(params));
 
-    print("STATUS => ${result.statusCode}");
-    print("BODY => ${result.body}");
-    Map resultBody = json.decode(result.body);
+      print("STATUS => ${result.statusCode}");
+      print("BODY => ${result.body}");
+      Map resultBody = json.decode(result.body);
 
-    if (result.statusCode == 200 || result.statusCode == 201) {
-      return ApiResponse.ok(User.fromJson(resultBody));
+      if (result.statusCode == 200 || result.statusCode == 201) {
+        return ApiResponse.ok(User.fromJson(resultBody));
+      }
+      return ApiResponse.error(resultBody["error"]);
+    } catch(error, exception){
+      print("Login error $error => $exception");
+      return ApiResponse.error("Login error $error => $exception");
     }
-    return ApiResponse.error(resultBody["error"]);
   }
 }
