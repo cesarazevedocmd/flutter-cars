c05640bd169e9eaacca088edc7c35f328fa3f192
Cesar Alves de Azevedo <cesarazevedocmd@gmail.com>
2021-05-04 14:12:26 -0300

    Creating request with Headers

--- lib/pages/login_api.dart
+++ lib/pages/login_api.dart
@@ -1,12 +1,19 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class LoginApi {
   static Future<bool> login(String login, String password) async {
-    var url = "http://livrowebservices.com.br/rest/login";
+    var url = "https://carros-springboot.herokuapp.com/api/v2/login";
 
-    var params = {"login": login, "senha": password};
+    Map params = {"username": login, "password": password};
 
-    var response = await http.post(Uri.parse(url), body: params);
+    Map<String, String> headers = {"Content-Type": "application/json"};
+
+    var response = await http.post(Uri.parse(url), headers: headers, body: json.encode(params));
+
+    print("STATUS => ${response.statusCode}");
+    print("BODY => ${response.body}");
 
     return response.statusCode == 200 || response.statusCode == 201;
   }
