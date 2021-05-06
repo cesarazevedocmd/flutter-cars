94461f3ddffa3e80aedc7ad66d6bd1108cbc8e86
Cesar Alves de Azevedo <cesarazevedocmd@gmail.com>
2021-05-06 00:41:16 -0300

    Improving header with FutureBuilder

--- lib/car/drawer_list.dart
+++ lib/car/drawer_list.dart
@@ -1,4 +1,5 @@
 import 'package:car_project/login/login_page.dart';
+import 'package:car_project/login/user.dart';
 import 'package:car_project/login/user_manager.dart';
 import 'package:car_project/util/nav.dart';
 import 'package:flutter/material.dart';
@@ -9,20 +10,23 @@ class DrawerList extends StatefulWidget {
 }
 
 class _DrawerListState extends State<DrawerList> {
-  String _name = "";
-  String _email = "";
-  String _urlPhoto = "";
-  bool _userNotLoaded = true;
-
   @override
   Widget build(BuildContext context) {
-    _loadUser();
-
     return SafeArea(
       child: Drawer(
         child: ListView(
           children: [
-            _userNotLoaded ? _loader() : _header(),
+            FutureBuilder<User>(
+                future: UserManager.getUser(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData && snapshot.data != null) return _header(snapshot.data);
+
+                  return Center(
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation(Colors.white),
+                    ),
+                  );
+                }),
             ListTile(
               leading: Icon(Icons.logout),
               title: Text("Sign out"),
@@ -42,31 +46,16 @@ class _DrawerListState extends State<DrawerList> {
     );
   }
 
-  UserAccountsDrawerHeader _header() {
+  UserAccountsDrawerHeader _header(User user) {
     return UserAccountsDrawerHeader(
-      accountName: Text(_name),
-      accountEmail: Text(_email),
-      currentAccountPicture: CircleAvatar(backgroundImage: _urlPhoto.isEmpty ? null : NetworkImage(_urlPhoto)),
+      accountName: Text(user.nome),
+      accountEmail: Text(user.email),
+      currentAccountPicture: CircleAvatar(backgroundImage: NetworkImage(user.urlFoto)),
     );
   }
 
   _onClickLogout(BuildContext context) {
     Navigator.pop(context);
     push(context, LoginPage(), replace: true);
-  }
-
-  void _loadUser() {
-    if (_userNotLoaded) {
-      UserManager.getUser().then((user) {
-        if (user != null) {
-          setState(() {
-            _name = user.nome;
-            _email = user.email;
-            _urlPhoto = user.urlFoto;
-          });
-          _userNotLoaded = false;
-        }
-      });
-    }
   }
 }
