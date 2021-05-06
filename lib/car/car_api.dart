4d863ac1c0eebaea2ac21d5c1e1026f6359be277
Cesar Alves de Azevedo <cesarazevedocmd@gmail.com>
2021-05-06 00:26:47 -0300

    Getting token from user saved and adding into the car request

--- lib/car/car_api.dart
+++ lib/car/car_api.dart
@@ -2,16 +2,15 @@ import 'dart:convert';
 
 import 'package:car_project/api/api_response.dart';
 import 'package:car_project/car/car_type.dart';
+import 'package:car_project/login/user_manager.dart';
 import 'package:http/http.dart' as http;
 
 import 'car.dart';
 
 class CarApi {
   static Future<ApiResponse<List<Car>>> loadCars(CarType type) async {
-    var url = Uri.parse("https://carros-springboot.herokuapp.com/api/v1/carros/tipo/${type.getType()}");
-    print("GET >> $url");
-    final token =
-        "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTYyMTAyMTQ3Niwicm9sIjpbIlJPTEVfQURNSU4iXX0.eny5cc2CZfFgdmtE7cTGl6edr42h_rp3jt7uHVoUWIah15dkH3YsaJGxIerEbz5Mf_AHKEyusd7Cknfz9hBWhQ";
+    var url = Uri.parse("https://carros-springboot.herokuapp.com/api/v2/carros/tipo/${type.getType()}");
+    final token = (await UserManager.getUser()).token;
     var headers = {"Content-Type": "application/json", "Authorization": "Bearer $token"};
 
     var response = await http.get(url, headers: headers);
