cbec7abf34426d571ff0dee38064501153fedc7e
Cesar Alves de Azevedo <cesarazevedocmd@gmail.com>
2021-05-19 22:58:48 -0300

    Creating actions (options in toolbar) and PopupMenuButton

--- lib/car/car_details_page.dart
+++ lib/car/car_details_page.dart
@@ -11,6 +11,20 @@ class CarDetailsPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(_car.nome),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.place),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.videocam),
+          ),
+          PopupMenuButton<MyPopupMenuItem>(
+            onSelected: (item) => item.performClick(),
+            itemBuilder: (context) => MyPopupMenuItem.getPopupOptions(),
+          ),
+        ],
       ),
       body: _body(),
     );
@@ -18,5 +32,22 @@ class CarDetailsPage extends StatelessWidget {
 
   _body() {
     return Image.network(_car.urlFoto);
+  }
+}
+
+class MyPopupMenuItem {
+  String value;
+  Function _click;
+
+  MyPopupMenuItem(this.value, this._click);
+
+  performClick() => this._click.call();
+
+  static List<PopupMenuItem<MyPopupMenuItem>> getPopupOptions() {
+    return [
+      PopupMenuItem(value: MyPopupMenuItem("Edit", () => print("Edit Clicked")), child: Text("Edit")),
+      PopupMenuItem(value: MyPopupMenuItem("Delete", () => print("Delete Clicked")), child: Text("Delete")),
+      PopupMenuItem(value: MyPopupMenuItem("Share", () => print("Share Clicked")), child: Text("Share"))
+    ];
   }
 }
