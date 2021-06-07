438b50c8129d5ca580283c1b2278e9e11e08044d
Cesar Alves de Azevedo <cesarazevedocmd@gmail.com>
2021-06-07 10:56:08 -0300

    Creating function to upload image in CarApi

--- lib/car/manager/car_api.dart
+++ lib/car/manager/car_api.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:car_project/api/api_response.dart';
 import 'package:car_project/car/entity/car_type.dart';
 import 'package:car_project/login/user_manager.dart';
 import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
+import 'package:path/path.dart' as path;
 
 import '../entity/car.dart';
 
@@ -15,7 +16,7 @@ class CarApi {
 
       Map headers = await getHeader();
 
-      Response response = await http.get(url, headers: headers);
+      http.Response response = await http.get(url, headers: headers);
 
       List bodyList = json.decode(response.body);
 
@@ -36,7 +37,7 @@ class CarApi {
 
       Map headers = await getHeader();
 
-      Response response = await (car.id == null
+      http.Response response = await (car.id == null
           ? http.post(Uri.parse(url), headers: headers, body: car.toJson())
           : http.put(Uri.parse(url), headers: headers, body: car.toJson()));
 
@@ -44,8 +45,6 @@ class CarApi {
         Map bodyMap = json.decode(response.body);
 
         Car car = Car.fromMap(bodyMap);
-
-        print("NEW CAR: $car}");
 
         return ApiResponse.ok(true);
       }
@@ -67,10 +66,38 @@ class CarApi {
 
       Map headers = await getHeader();
 
-      Response response = await http.delete(Uri.parse(uri), headers: headers);
+      http.Response response = await http.delete(Uri.parse(uri), headers: headers);
 
       if (response.statusCode == 200 && response.body != null && response.body.isNotEmpty) return ApiResponse.ok(true);
       return ApiResponse.error("Car not deleted");
+    } catch (error) {
+      return ApiResponse.error(error);
+    }
+  }
+
+  static Future<ApiResponse<String>> uploadImage(File imageFile) async {
+    try {
+      Uri uri = Uri.parse("https://carros-springboot.herokuapp.com/api/v1/upload");
+
+      Map headers = await getHeader();
+
+      List<int> imageBytes = imageFile.readAsBytesSync();
+      String base64 = base64Encode(imageBytes);
+      String name = path.basename(imageFile.path);
+
+      Map<String, String> body = {"fileName": name, "mimeType": "image.jpeg", "base64": base64};
+      String jsonBody = json.encode(body);
+
+      http.Response response = await http.post(uri, headers: headers, body: jsonBody);
+
+      if ((response.statusCode == 200 || response.statusCode == 201) &&
+          response.body != null &&
+          response.body.isNotEmpty) {
+        Map result = json.decode(response.body);
+
+        return ApiResponse.ok(result["url"]);
+      }
+      return ApiResponse.error("fail to upload image");
     } catch (error) {
       return ApiResponse.error(error);
     }
